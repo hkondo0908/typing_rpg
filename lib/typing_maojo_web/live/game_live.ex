091bad10780c0f31745d98b6dc9f1dde8c80a641
近藤整 <hitoshi@kondouseinoAir.nrima1.kt.home.ne.jp>
2021-07-01 15:49:36 +0900

    typing part not use DB

---
+++ lib/typing_maojo_web/live/game_live.ex
@@ -0,0 +1,51 @@
+defmodule TypingMaojoWeb.GameLive do
+    use Phoenix.LiveView
+    use Phoenix.HTML 
+    import TypingMaojoWeb.ErrorHelpers
+
+    @timeout Float.round(1_000_000 / 60) |> trunc
+    def mount(_params,_session,socket) do
+        time = :erlang.system_time
+        connected?(socket) && MicroTimer.send_every(@timeout, :update)
+        new_socket = assign(socket, [ch: "", sentence: Enum.random(0..5), num: 0, pch: "-", time: time])
+        {:ok, update_socket(new_socket)}
+    end
+
+    def handle_info(:update, socket) do
+        {:noreply, update_socket(socket)}
+    end
+
+    def handle_event("change",%{"fight"=>fight,"forward"=>forward, "for"=>forw, "finish"=>finish},socket) do
+        text = fight
+        pre_text = String.replace(fight,forward,"")
+        finish = String.to_integer(finish)
+        number = socket.assigns.num
+        new_socket =
+        if pre_text == forw and number == (finish-1) do
+            assign(socket, sentence: Enum.random(0..5))
+            |> assign(num: 0) 
+        else
+            if pre_text == forw do
+            update(socket, :num, &(&1 + 1))
+            else
+            socket
+            end
+        end
+        {:noreply, assign(new_socket, ch: text, pch: forward)}
+    end
+
+    defp update_socket(socket) do
+        socket
+        |> assign(:erlang_system_time, get_erlang_system_time())
+        |> assign(:date_system_time, get_date_system_time())
+    end
+
+    defp get_erlang_system_time() do
+        :erlang.system_time
+    end
+
+    defp get_date_system_time() do
+    :erlang.system_time
+    |> DateTime.from_unix!(:native)
+    end
+end
