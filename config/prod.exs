cd0af274806dcfe9c7710c91b54fa20cbaafcf41
近藤整 <hitoshi@kondouseinoAir.nrima1.kt.home.ne.jp>
2021-07-01 17:46:04 +0900

    first commit

--- config/prod.exs
+++ config/prod.exs
@@ -12,6 +12,7 @@ use Mix.Config
 config :typing_maojo, TypingMaojoWeb.Endpoint,
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
+  check_origin: ["https://uniform-blueviolet-clingfish.gigalixirapp.com"]
 
 # Do not print debug messages in production
 config :logger, level: :info
