5bf12738c431cbce7ab112d648b06452ba4ec81f
endoakak <akendo2020@gmail.com>
2021-07-02 13:58:01 +0900

    time limit comp

--- lib/typing_maojo_web/live/game_live.ex
+++ lib/typing_maojo_web/live/game_live.ex
@@ -1,6 +1,6 @@
 defmodule TypingMaojoWeb.GameLive do
     use Phoenix.LiveView
-    use Phoenix.HTML 
+    use Phoenix.HTML
     import TypingMaojoWeb.ErrorHelpers
     alias TypingMaojoWeb.MakeList
 
@@ -9,7 +9,7 @@ defmodule TypingMaojoWeb.GameLive do
         time = :erlang.system_time
         connected?(socket) && MicroTimer.send_every(@timeout, :update)
         sentence = MakeList.ex_sentence(0)
-        new_socket = assign(socket, [num: 0, time: time, sentence: sentence, sentence_at: 0])
+        new_socket = assign(socket, [num: 0, time: time, sentence: sentence, sentence_at: 0, error_count: 0])
         {:ok, update_socket(new_socket)}
     end
 
@@ -27,16 +27,20 @@ defmodule TypingMaojoWeb.GameLive do
         if pre_text == forw and number == (finish-1) do
             assign(socket, sentence: MakeList.ex_sentence(at + 1))
             |> update(:sentence_at, &(&1 + 1))
-            |> assign(num: 0) 
+            |> assign(num: 0)
         else
             if pre_text == forw do
                 update(socket, :num, &(&1 + 1))
             else
-                socket
+                if pre_text == "Shift" do
+                    socket
+                else
+                    update(socket, :error_count, &(&1 + 1))
+                end
             end
         end
         sentence_num = new_socket.assigns.sentence_at
-        new_socket = 
+        new_socket =
         if sentence_num == list_length  do
             redirect(socket, to: "/game/finish")
         else
@@ -46,8 +50,12 @@ defmodule TypingMaojoWeb.GameLive do
     end
 
     defp update_socket(socket) do
-        socket
-        |> assign(:erlang_system_time, get_erlang_system_time())
+        socket = assign(socket, :erlang_system_time, get_erlang_system_time())
+        if div(get_erlang_system_time() - socket.assigns.time, 1000000000) > 60 do
+            redirect(socket, to: "/game/finish")
+        else
+            socket
+        end
     end
 
     defp get_erlang_system_time() do
